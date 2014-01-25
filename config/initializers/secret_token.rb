040ce9bf98b1402b5fa7c682a537a06777156ff2
Jonathan Mazin <jmazin@mshare.net>
2014-01-25 15:12:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sec::Application.config.secret_key_base = 'd538469ad0b3c25619ea56c78b580787e33791367afdeea993aa17f7d56fe3fbbbd40405df4e9b54b0a219a40091cc113868c4e25542be281d68736d3cc0e608'
