e21582e7b06f42c30e315f399dd6c193d2389cc9
Will Langstroth <will@langstroth.com>
2014-01-31 21:16:36 -0500

    Add application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def cents_to_dollars(cents)
+    number_to_currency(cents.to_d / 100)
+  end
+
+  def number_to_month(month_number)
+    I18n.t("date.month_names")[month_number.to_i]
+  end
+
 end
