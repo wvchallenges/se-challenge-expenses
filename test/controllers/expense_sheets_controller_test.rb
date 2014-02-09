a1ee5470342d3da43dcfa7dee818f9a06493547b
Will Langstroth <will@langstroth.com>
2014-02-09 14:26:59 -0500

    Move test from controller to model

--- test/controllers/expense_sheets_controller_test.rb
+++ test/controllers/expense_sheets_controller_test.rb
@@ -19,29 +19,6 @@ class ExpenseSheetsControllerTest < ActionController::TestCase
   end
 
   def test_upload
-    csv_rows = <<-EOS
-date,category,employee name,employee address,expense description,pre-tax amount,tax name,tax amount
-12/1/2013,Travel,Don Draper,"783 Park Ave, New York, NY 10021",Taxi ride, 350.00 ,NY Sales tax, 31.06
-12/15/2013,Meals and Entertainment,Steve Jobs,"1 Infinite Loop, Cupertino, CA 95014",Team lunch, 235.00 ,CA Sales tax, 17.63
-12/31/2013,Computer - Hardware,Jonathan Ive,"1 Infinite Loop, Cupertino, CA 95014",HP Laptop, 999.00 ,CA Sales tax, 74.93
-EOS
-
-    file = Tempfile.new('expenses.csv')
-    file.write(csv_rows)
-    file.rewind
-
-    assert_difference "ExpenseSheet.count", 1 do
-      assert_difference "Expense.count", 3 do
-        post :upload, file: Rack::Test::UploadedFile.new(file, 'text/csv')
-      end
-    end
-    expense = Expense.last
-    assert_equal Date.new(2013,12,31), expense.date
-    assert_equal 'Jonathan Ive', expense.employee_name
-    assert_equal 99900, expense.pre_tax_amount_cents
-    assert_equal 7493, expense.tax_amount_cents
-    assert_redirected_to expense_sheet_path(ExpenseSheet.last)
-    assert_equal "Expense sheet uploaded", flash[:notice]
   end
 
   def test_index
--- test/models/expense_sheet_test.rb
+++ test/models/expense_sheet_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ExpenseSheetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_from_file
+    csv_rows = <<-EOS
+date,category,employee name,employee address,expense description,pre-tax amount,tax name,tax amount
+12/1/2013,Travel,Don Draper,"783 Park Ave, New York, NY 10021",Taxi ride, 350.00 ,NY Sales tax, 31.06
+12/15/2013,Meals and Entertainment,Steve Jobs,"1 Infinite Loop, Cupertino, CA 95014",Team lunch, 235.00 ,CA Sales tax, 17.63
+12/31/2013,Computer - Hardware,Jonathan Ive,"1 Infinite Loop, Cupertino, CA 95014",HP Laptop, 999.00 ,CA Sales tax, 74.93
+EOS
+
+    file = Tempfile.new('expenses.csv')
+    file.write(csv_rows)
+    file.rewind
+
+    expense = Expense.last
+    assert_equal Date.new(2013,12,31), expense.date
+    assert_equal 'Jonathan Ive', expense.employee_name
+    assert_equal 99900, expense.pre_tax_amount_cents
+    assert_equal 7493, expense.tax_amount_cents
+  end
 end
