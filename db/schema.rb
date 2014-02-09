ef2bff6288abb1e53df84babf62870f2212f238a
Will Langstroth <will@langstroth.com>
2014-02-09 15:35:41 -0500

    Add employee_id to expenses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140209202220) do
+ActiveRecord::Schema.define(version: 20140209203416) do
 
   create_table "employees", force: true do |t|
     t.string   "name"
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 20140209202220) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "expense_sheet_id"
+    t.integer  "employee_id"
   end
 
+  add_index "expenses", ["employee_id"], name: "index_expenses_on_employee_id"
   add_index "expenses", ["expense_sheet_id"], name: "index_expenses_on_expense_sheet_id"
 
 end
