69bb9e940f6633e7b2f242befe6d2381d47cbff0
Chris Johnston <chris@fuzzylizard.com>
2014-06-07 13:38:28 -0400

    now shows montly expenses

--- expenseloader/app/controllers/expenses_controller.rb
+++ expenseloader/app/controllers/expenses_controller.rb
@@ -1,5 +1,13 @@
 class ExpensesController < ApplicationController
   def index
-    @expenses = Expense.all
+    @monthly_expenses = Expense.connection.select_all(%q{
+      select  sum(pre_tax_amount) as pre_tax_total,
+              sum(tax_amount) as tax_total,
+              strftime('%Y', purchase_date) as year,
+              strftime('%m', purchase_date) as month
+      from expenses
+      group by year, month
+      order by year, month
+    })
   end
 end
