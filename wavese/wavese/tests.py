f135f34286633241fd69c7b61add57caebf8b119
Dave Weber <weberd09@gmail.com>
2014-06-23 22:11:31 -0400

    Project Init
    Basic project file structure. Configured settings and URLs, initialized
    tests and MVC files

---
+++ wavese/wavese/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class Tests(TestCase):
+
+    def test_normal_case(self):
+        """
+        Normal case should pass
+        """
+        pass
+
+    def test_bad_case(self):
+        """
+        Problematic case should fail
+        """
+        pass
