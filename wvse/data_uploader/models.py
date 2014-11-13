0a2aac6ef45ef2a263b9278baf4234c7daeb4319
Jero Sutlovic <jsutlovic@gmail.com>
2014-11-13 15:32:25 +0000

    Create Employee and Expense models

--- wvse/data_uploader/models.py
+++ wvse/data_uploader/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=60)
+    address = models.CharField(max_length=120)
+
+
+class Expense(models.Model):
+    date = models.DateField()
+    category = models.CharField(max_length=60)
+    employee = models.ForeignKey('Employee')
+    description = models.CharField(max_length=120)
+    pre_tax_amt = models.DecimalField(max_digits=7, decimal_places=2)
+    tax_name = models.CharField(max_length=12)
+    tax_amount = models.DecimalField(max_digits=6, decimal_places=2)
