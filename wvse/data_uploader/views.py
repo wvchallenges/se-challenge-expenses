b40f36cd791f7e833cc599d6d5fba276ebf98068
Jero Sutlovic <jsutlovic@gmail.com>
2014-11-13 17:22:48 +0000

    Basic view to handle file form upload

--- wvse/data_uploader/views.py
+++ wvse/data_uploader/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from forms import UploadDataFileForm
+
+
 # Create your views here.
+def upload(request):
+    if request.method == "POST":
+        form = UploadDataFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # TODO: handle form
+            pass
+    else:
+        form = UploadDataFileForm()
+
+    return render(request, 'data_uploader/upload.html', {'form': form})
