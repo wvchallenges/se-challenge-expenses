935283dcc3b8c0856b8835d3409d654c99560def
Jero Sutlovic <jsutlovic@gmail.com>
2014-11-13 17:23:09 +0000

    Url routes

--- wvse/wvse/urls.py
+++ wvse/wvse/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'wvse.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^uploader/', include('data_uploader.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
