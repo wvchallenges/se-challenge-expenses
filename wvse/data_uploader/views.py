57dcbec581527863838af1d5d45eb1f8d041da03
Jero Sutlovic <jsutlovic@gmail.com>
2014-11-14 00:10:42 +0000

    Consistent quotes, add month_sums to render context

--- wvse/data_uploader/views.py
+++ wvse/data_uploader/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 
 from forms import UploadDataFileForm
+from logic import handle_uploaded_file
 
 
 # Create your views here.
 def upload(request):
-    if request.method == "POST":
+    month_sums = {}
+    if request.method == 'POST':
         form = UploadDataFileForm(request.POST, request.FILES)
         if form.is_valid():
-            # TODO: handle form
-            pass
+            month_sums = handle_uploaded_file(request.FILES['file'])
     else:
         form = UploadDataFileForm()
 
-    return render(request, 'data_uploader/upload.html', {'form': form})
+    return render(
+        request,
+        'data_uploader/upload.html',
+        {'form': form, 'month_sums': month_sums})
