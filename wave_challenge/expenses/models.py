ed46441f6d17e1e3c0ef5947489e9bc9c7bf1829
Willie Gollino <wgollino@yahoo.com>
2015-03-03 16:49:17 -0500

    initial db models

---
+++ wave_challenge/expenses/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class ExpenseCategory(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return self.name
+
+
+class TaxType(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Expense(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    expense_date = models.DateTimeField()
+    category = models.ForeignKey(ExpenseCategory)
+    employee = models.ForeignKey(Employee)
+    description = models.TextField()
+    pretax_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    tax = models.ForeignKey(TaxType)
+    tax_amount = models.DecimalField(max_digits=10, decimal_places=2)
