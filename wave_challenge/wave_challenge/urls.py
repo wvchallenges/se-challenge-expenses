5daab1a5f51e0fc233a1d43508ccf9c2d6124c1e
Willie Gollino <wgollino@yahoo.com>
2015-03-04 21:09:21 -0500

    added missing changes

--- wave_challenge/wave_challenge/urls.py
+++ wave_challenge/wave_challenge/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wave_challenge.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^expenses/', include('expenses.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
