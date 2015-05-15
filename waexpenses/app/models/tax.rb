e506f5407da79ba664d12ae1de2386da0fdc898c
Jim Graham <james.a.graham+github@gmail.com>
2015-05-15 09:21:22 -0400

    Show total paid to each tax

--- waexpenses/app/models/tax.rb
+++ waexpenses/app/models/tax.rb
@@ -1,4 +1,12 @@
 class Tax < ActiveRecord::Base
   has_and_belongs_to_many :expenses
   has_many :tax_amounts
+
+  def calculate_total()
+    total = 0.0
+    self.expenses.each do |e|
+      total += e.tax_amounts[0].amount
+    end
+    return total
+  end  
 end
