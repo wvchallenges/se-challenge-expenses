0f3f28f83e56bc65689523a030a6bf90dac86467
taesu <limtaesu@gmail.com>
2015-09-17 23:58:04 -0400

    .

--- exptracker/expense/views.py
+++ exptracker/expense/views.py
@@ -4,7 +4,14 @@ from models import Expense
 # Create your views here.
 def index(request):
     expenses = Expense.objects.all()
-    return render(request, 'index.html', {'expenses': expenses})
+    monthly_summary = [0]*12
+
+    for i in expenses:
+        month = i.date.month-1
+        amount = round(float(i.pretax) + float(i.tax),2)
+        print amount
+        monthly_summary[month] += amount
+    return render(request, 'index.html', {'expenses': expenses, 'monthly_summary':enumerate(monthly_summary)})
 
 def purge(request):
     Expense.objects.all().delete()
