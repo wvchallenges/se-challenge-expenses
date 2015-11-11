2f2fb7c1c5156b047a6056fd751fb861649d5504
Sarah <sarah.marie.ferguson@gmail.com>
2015-11-11 16:43:46 -0500

    Added get monthly expenses method to the expenses model class

--- expenses/import_file/models.py
+++ expenses/import_file/models.py
@@ -39,6 +39,19 @@ class Expenses(models.Model):
     def __str__(self):
         return self.expense_description
 
+    @staticmethod
+    def get_monthly_expenses(file_id):
+        file_object = File.objects.filter(id=file_id)
+        dates = Expenses.objects.filter(upload_file=file_object).dates('date', 'month')
+        monthly_expenses_dict = {}
+        for date in dates:
+            amount_dict = Expenses.objects.filter(upload_file=file_object, date__month=date.month, date__year=date.year).aggregate(Sum('tax_amount'), Sum('pre_tax_amount'))
+            tax_amount = amount_dict['tax_amount__sum']
+            pre_tax_amount = amount_dict['pre_tax_amount__sum']
+            monthly_expenses_dict.update({date: pre_tax_amount + tax_amount})
+        return monthly_expenses_dict
+
+
 
 
 
