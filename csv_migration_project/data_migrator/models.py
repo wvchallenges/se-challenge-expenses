7e5dba6bf97b4678509cbe5f8ed596bc2634bdd1
Tsering <hour1hr@gmail.com>
2015-11-13 19:59:52 -0500

    Added models for Employee, ExpenseCategory, and EmployeeExpenseModel

--- csv_migration_project/data_migrator/models.py
+++ csv_migration_project/data_migrator/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    employee_name       = models.CharField(max_length=255)
+    employee_address    = models.CharField(max_length=512)
+
+class ExpenseCategory(models.Model):
+    category_name       = models.CharField(max_length=255)
+
+class EmployeeExpenseModel(models.Model):
+    # ForeignKey fields
+    employee            = models.ForeignKey('data_migrator.Employee')
+    expense_category    = models.ForeignKey('data_migrator.ExpenseCategory')
+
+    # Date variables
+    created_date        = models.DateTimeField(auto_now_add=True)
+    expense_date        = models.DateTimeField()
+
+    # Float fields
+    pre_tax_amount      = models.DecimalField(decimal_places=2)
+    tax_amount          = models.DecimalField(decimal_places=2)
+    
+    # Char fields
+    expense_discription = models.CharField(max_length=1024)
+    tax_name            = models.CharField(max_length=255)
