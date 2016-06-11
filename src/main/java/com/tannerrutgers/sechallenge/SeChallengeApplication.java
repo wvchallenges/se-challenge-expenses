a6e1fdfa4299cf0e536808b3c97379e99cd81b75
Tanner Rutgers <tanner_rutgers@intuit.com>
2016-06-10 22:08:06 -0700

    Base/shell service

---
+++ src/main/java/com/tannerrutgers/sechallenge/SeChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.tannerrutgers.sechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeChallengeApplication.class, args);
+	}
+}
