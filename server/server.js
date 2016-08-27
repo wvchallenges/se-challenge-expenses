8de79d2196b9e9ee330cca3a6ce02c9d9320573c
dan523 <danwright523@gmail.com>
2016-08-27 12:25:11 -0400

    Simple form to upload csv

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import multer from 'multer' // express middleware for retrieving form data
 
 const app = express();
+const upload = multer();
 
-app.get('/', (req, res) => {
-    res.send('Hello from express');
-});
+app.post('/upload', upload.array('csvfiles'), (req, res) => {
+  console.log(req.files); 
+  res.send('ok'); 
+}); 
 
 const server = app.listen(3000, () => {
     console.log('Express is listening to http://localhost:3000');
