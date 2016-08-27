07382c5dcef73da731de29d78a2a3236f1867249
dan523 <danwright523@gmail.com>
2016-08-27 11:06:54 -0400

    Babel config for ES2016 and basic express setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-console.log('Server started');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from express');
+});
+
+const server = app.listen(3000, () => {
+    console.log('Express is listening to http://localhost:3000');
+});
