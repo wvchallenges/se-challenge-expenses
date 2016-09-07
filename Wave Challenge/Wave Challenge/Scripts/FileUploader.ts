6b085639cb208f90558323eaae4a5958af53a2fa
AndrePlatov <platovandrew@gmail.com>
2016-09-06 22:32:31 -0400

    Round 1  - upload view draft

---
+++ Wave Challenge/Wave Challenge/Scripts/FileUploader.ts
@@ -0,0 +1,29 @@
+﻿$().ready(function () {
+
+    function handleFileImport() {
+        // TODO: extract code specific to Expense Import to its own module
+        var selectedCsv = (<HTMLInputElement>$("#sourceFileInput")[0]).files[0];
+
+        if (selectedCsv) {
+            var output = [];
+            var fr = new FileReader();
+            fr.onload = function (e: any) {
+                // e.target.result should contain the text
+                var text = e.target.result;
+
+                var lines = text.split(/[\r\n]+/g); // tolerate both Windows and Unix linebreaks
+
+                for (var i = 0; i < lines.length; i++) {
+                    output.push('<li>' + lines[i] + '<br>');
+                }
+
+                document.getElementById('file').innerHTML = 'File contents:<ul>' + output.join('') + '</ul>';
+
+            };
+            fr.readAsText(selectedCsv);
+        }
+        else {
+            // TODO: handle
+        }
+    }
+});
