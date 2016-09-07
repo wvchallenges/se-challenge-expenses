6ffb25cac212283fe416060c66a4f057b6de05e3
Matthew Ouyang <matthew@bluenotion.com>
2016-09-07 11:32:31 -0400

    basic csv processing
    - process csv input + handle binary input using TextIOStream
    - basic web form
    - add expense namespace

--- wavese/expense/views.py
+++ wavese/expense/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import Http404
+from django.http.response import HttpResponse
+
+from .models import CSVItemReader
+from io import TextIOWrapper
+
+batchInterval = 5
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if (request.method == 'GET'): 
+        return render(request, 'expense/index.html', {})
+    elif (request.method == 'POST'): 
+        csvItemReader = CSVItemReader(TextIOWrapper(request.FILES['expenses'].file, "utf-8"), numHeaderRows=1)
+        batch = []
+        for row in csvItemReader:
+            batch.append(row)
+            if (len(batch) >= batchInterval):
+                processBatch(batch)
+                batch = []
+        processBatch(batch)
+        return HttpResponse(batch)
+    else:
+        raise Http404()
+
+def processBatch(batch):
+    pass
--- wavese/wavese/urls.py
+++ wavese/wavese/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^expense/', include('expense.urls')),
+    url(r'^expense/', include('expense.urls', namespace='expense')),
     url(r'^admin/', admin.site.urls),
 ]
