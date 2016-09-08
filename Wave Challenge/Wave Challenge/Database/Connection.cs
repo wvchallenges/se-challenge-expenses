ad718c9fa70e3fc3075cc4fbe20d267223156ac5
AndrePlatov <platovandrew@gmail.com>
2016-09-08 18:21:13 -0400

    Created Connection Class for database access.

---
+++ Wave Challenge/Wave Challenge/Database/Connection.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+using System;
+using System.Configuration;
+
+namespace Wave_Challenge.Database
+{
+    public class Connection : IDisposable
+    {
+        public NpgsqlConnection conn { get; set; }
+
+        public Connection()
+        {
+            conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["NpgsqlConnection"].ConnectionString);
+        }
+
+        public void Dispose()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+    }
+}
---
+++ Wave Challenge/Wave Challenge/Database/ExpenseImportData.cs
@@ -0,0 +1,37 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Wave_Challenge.Models;
+using Dapper;
+using System.Threading.Tasks;
+
+namespace Wave_Challenge.Database
+{
+    public class ExpenseImportData : Connection
+    {
+        public ExpenseImportData() : base() { }
+        
+        public async Task<ImportResult> ImportEmployeeExpenses(IEnumerable<ExpenseRecord> records)
+        {
+            ImportResult result = new ImportResult();
+
+            string sql = @"
+INSERT INTO public.""EmployeeExpense"" (""Date"", ""Category"", ""EmployeeName"", ""EmployeeAddress"", ""ExpenseDescription"", 
+       ""PreTaxAmount"", ""TaxName"", ""TaxAmount"")
+VALUES (@Date, @Category, @EmployeeName, @EmployeeAddress, @ExpenseDescription, 
+       @PreTaxAmount, @TaxName, @TaxAmount)
+";
+            try
+            {
+                await conn.ExecuteAsync(sql, records);
+            }
+            catch (Exception e) {
+                result.Success = false;
+                result.Exception = e;
+            }
+
+            return result;
+        }
+    }
+}
