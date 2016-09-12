37e9edaa485eb904e06bba09372758565afbdcbf
Matthew Ouyang <matthew@bluenotion.com>
2016-09-12 16:08:17 -0400

    render expenses by month in html
    - add totalExpenseAmount = preTaxAmount + taxAmount to returned model

--- wavese/expense/views.py
+++ wavese/expense/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
+from django.db import models
 from django.db.models import Sum
 from django.db.models.functions import TruncMonth
 from django.http import Http404
-from django.http.response import HttpResponse
 
 from .models import CSVItemReader, CSVRowToExpenseModelProcessor, ExpenseItemWriter, Job
 from .models import Expense
@@ -23,11 +23,13 @@ def index(request):
             batchInterval=defaultBatchInterval, params=params)
         job.save()
         job.run()
-        querySet = Expense.objects.filter(job=job) \
+        monthlyExpenses = Expense.objects.filter(job=job) \
             .annotate(truncMonth=TruncMonth('date')) \
             .values('truncMonth') \
-            .annotate(preTaxAmount=Sum('preTaxAmount'), taxAmount=Sum('taxAmount'))
-        return HttpResponse(querySet)
+            .annotate(preTaxAmount=Sum('preTaxAmount'), taxAmount=Sum('taxAmount'), 
+                totalExpenseAmount=Sum('preTaxAmount') + Sum('taxAmount'))
+        context = {'monthlyExpenses' : monthlyExpenses}
+        return render(request, 'expense/monthlyExpenses.html', context)
     else:
         raise Http404()
 
