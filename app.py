2c7903eeda7e0b05c4431f0df707ac796ddbde02
Aaron Lam <aaron.chihon.lam@gmail.com>
2016-09-26 20:41:37 -0400

    file upload code and html markup

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/home/aaron/Projects/wave-interview/uploads'
+UPLOAD_FOLDER = '/tmp'
 ALLOWED_EXTENSIONS = set(['csv'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
-    return filename.split['.'][-1] in ALLOWED_EXTENSIONS
+    return filename.split('.')[-1] in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
-def main():
+def upload_file():
     if request.method == 'POST':
         # check if post has a file part
         if 'file' not in request.files:
             return redirect(request.url)
+
         f = request.files['file']
+
         # empty file name
         if f and f.filename == '':
             return redirect(request.url)
+
         if f and allowed_file(f.filename):
             filename = secure_filename(f.filename)
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return render_template('main.html')
+            return redirect(request.url)
 
+    return render_template('main.html')
