339064a5c04c9c874fb65841aca00daae431dc0a
Aaron Lam <aaron.chihon.lam@gmail.com>
2016-09-27 00:28:17 -0400

    Compute monthly expenses

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 import csv
 from datetime import datetime
+from itertools import groupby
+
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
+from sqlalchemy import func
 
 UPLOAD_FOLDER = '/tmp'
 ALLOWED_EXTENSIONS = set(['csv'])
@@ -27,6 +30,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testing.db'
 db = SQLAlchemy(app)
 
 class Expense(db.Model):
+    '''
+        Expense model
+    '''
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date = db.Column(db.DateTime)
     category = db.Column(db.String(MAX_STR_LEN))
@@ -62,6 +68,12 @@ def allowed_filename(filename):
     '''
     return filename.split('.')[-1] in ALLOWED_EXTENSIONS
 
+def month_year_grouper(item):
+    '''
+        Return the (month, year) tuple for the given item.
+    '''
+    return item.date.month, item.date.year
+
 def read_file_to_db(csv_file):
     '''
         Read the file into database.
@@ -81,6 +93,31 @@ def read_file_to_db(csv_file):
         db.session.add(expense)
     db.session.commit()
 
+
+def query_monthly_expense():
+    '''
+        Query monthly expenses from database.
+
+        Return a list of ("mm/yyyy", pretax_amount, tax_amount) tuples sorted
+        chronologically.
+    '''
+    expenses = db.session.query(
+        Expense.date,
+        Expense.pretax_amount,
+        Expense.tax_amount,
+    ).order_by(Expense.date).all()
+
+    monthly_expenses = []
+    for (month, year), items in groupby(expenses, month_year_grouper):
+        pretax_amount = 0
+        tax_amount = 0
+        for i in items:
+            pretax_amount += i.pretax_amount
+            tax_amount += i.tax_amount
+        monthly_expenses.append(((month, year), pretax_amount, tax_amount))
+
+    return monthly_expenses
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     '''
@@ -94,10 +131,12 @@ def upload_file():
 
         f = request.files['file']
 
-        # empty file name
+        # check empty file name
         if f and f.filename == '':
             return redirect(request.url)
 
         read_file_to_db(f)
+
+    expenses = query_monthly_expense()
 
     return render_template('main.html')
