cc9eea41fbf0d0574fa288a141c5e15e1e876d5d
Lawrence Wong <lawrencecywong@gmail.com>
2016-10-01 16:11:01 -0400

    Setting up Models and having the first migration run to creat the tables

--- csvChallenge/waveCSVChallenge/models.py
+++ csvChallenge/waveCSVChallenge/models.py
@@ -2,5 +2,25 @@ from django.db import models
 
 
 class Employees(models.Model):
-    fullname_text = models.CharField(max_length=200)
-    address_text = models.CharField(max_length=200)
+    full_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+
+class ExpenseCategories(models.Model):
+    category_name = models.CharField(max_length=200)
+
+
+class TaxInformation(models.Model):
+    tax_name = models.CharField(max_length=100)
+    tax_rate = models.DecimalField(max_digits=6, decimal_places=4)
+
+
+class Expenses(models.Model):
+    category = models.ForeignKey(ExpenseCategories)
+    employee = models.ForeignKey(Employees)
+    tax = models.ForeignKey(TaxInformation)
+    date = models.DateTimeField()
+    description = models.CharField(max_length=250)
+    pre_tax_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    tax_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total_amount = models.DecimalField(max_digits=15, decimal_places=2)
