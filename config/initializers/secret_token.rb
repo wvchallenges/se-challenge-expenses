230cbce7d070adf6ddbdce737acd5791106d03dc
Julia <jbronfenbrener@gmail.com>
2016-10-02 23:01:19 -0400

    initial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waveblock::Application.config.secret_token = '4b7f7f805d655835629471f88b54c58a7a887969deb5aeff07cc818f5d0ac9f9e8c5a3f0b93c92901485028a5db54c4d3af592429f35b0f5b2bf1e8d7af5a43d'
