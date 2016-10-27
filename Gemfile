0f1141da0432d42c18baaf39ff7f83e45000218d
David Mills <dave@1morepx.com>
2016-10-27 13:31:05 -0400

    Adding DatabaseCleaner

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem "factory_girl_rails", "~> 4.0"
 gem 'guard'
 gem 'guard-rspec'
 
+group :test do
+  gem 'database_cleaner'
+end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'support/factory_girl'
+require 'support/database_cleaner'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
