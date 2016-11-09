63127c0a3f9cfe2d252a7f0319b29388a72a8c82
Zi <hungryzi@gmail.com>
2016-11-09 19:40:47 +0100

    Added upload spec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
 class PagesController < ApplicationController
   def home; end
+
+  def upload; end
 end
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,6 +1,14 @@
 feature 'Homepage' do
-  scenario 'see Hello' do
+  let(:fixture_file_path) { Rails.root.join('spec/fixtures/simple_example.csv') }
+
+  scenario 'can upload a file' do
     visit '/'
-    expect(page).to have_content('Hello')
+
+    attach_file('file', fixture_file_path)
+    click_on 'Upload'
+
+    within '[data-test=test-month-201312]' do
+      expect(page).to have_content('585.00')
+    end
   end
 end
