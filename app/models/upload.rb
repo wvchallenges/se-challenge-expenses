ddcf42c8c739fd3acb09e86011df38c12652d2b5
Zi <hungryzi@gmail.com>
2016-11-11 20:44:37 +0100

    Allowed duplicate file name

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,8 +2,7 @@ class Upload < ActiveRecord::Base
   has_many :expenses
 
   validates :file_name,
-            presence: true,
-            uniqueness: true
+            presence: true
 
   default_scope { order('created_at DESC') }
 end
