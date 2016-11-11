16e7f49817bc65f3a429fbdde3d69a454045ba32
Zi <hungryzi@gmail.com>
2016-11-11 19:42:52 +0100

    Added controller specs to make sure Transaction works

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,31 @@
+describe PagesController do
+  describe '#upload' do
+    let(:file) { fixture_file_upload(fixture_file_path, 'text/csv') }
+
+    subject { post :upload, params: { file: file } }
+
+    context 'when valid' do
+      let(:fixture_file_path) { 'simple_example.csv' }
+
+      it 'creates 1 upload' do
+        expect { subject }.to change(Upload, :count).by(1)
+      end
+
+      it 'imports any expenses' do
+        expect { subject }.to change(Expense, :count).by(4)
+      end
+    end
+
+    context 'when invalid' do
+      let(:fixture_file_path) { 'invalid_example.csv' }
+
+      it "doesn't create any uploads" do
+        expect { subject }.to change(Upload, :count).by(0)
+      end
+
+      it "doesn't import any expense" do
+        expect { subject }.to change(Expense, :count).by(0)
+      end
+    end
+  end
+end
