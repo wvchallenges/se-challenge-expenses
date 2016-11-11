f8be4fec2baa1aaef5cec24231fdb0e7526dfb8b
Zi <hungryzi@gmail.com>
2016-11-11 12:45:08 +0100

    Refactored with MonthlyReportPresenter

---
+++ app/presenters/monthly_report_presenter.rb
@@ -0,0 +1,24 @@
+class MonthlyReportPresenter < ApplicationPresenter
+  include MoneyRails::ActionViewExtension
+
+  def initialize(year_month_str, data)
+    @year_month_str = year_month_str
+    @data = data
+  end
+
+  def month
+    year_month_str
+  end
+
+  def pre_tax_amount
+    humanized_money(data[:pre_tax_amount])
+  end
+
+  def tax_amount
+    humanized_money(data[:tax_amount])
+  end
+
+  private
+
+  attr_reader :data, :year_month_str
+end
