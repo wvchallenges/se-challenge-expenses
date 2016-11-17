02b260dcef6e9bfb4217c1fbf8a11363b17d4395
Lee Mordell <lee.mordell@gmail.com>
2016-11-17 11:49:16 -0800

    [pull] can accept file on backend

--- Public/app/app.jsx
+++ Public/app/app.jsx
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
+import $ from 'jquery'
 
 
 export default class App extends Component {
 	constructor(props) {
 		super(props)
-
 		this.submitFile = this.submitFile.bind(this)
 	}
 
 	submitFile() {
-		console.log('works')
+		const data = new FormData()
+		data.append('file', document.getElementById('file').files[0])
+
+		var settings = {
+		  "async": true,
+		  "crossDomain": true,
+		  "url": "/api/uploadFile",
+		  "method": "POST",
+		  "headers": {
+		    "cache-control": "no-cache"
+		  },
+		  "processData": false,
+		  "contentType": false,
+		  "mimeType": "multipart/form-data",
+		  "data": data
+		}
+
+		$.ajax(settings).done(function (response) {
+		  console.log(response);
+		})
 	}
 
 	render() {
 		return (
 		<div>
-			<h1>It works!</h1>
-			<input type='file' />
+			<h1>Wave Software Engineer Coding Challenge</h1>
+			<input type='file' id='file' name='file'/>
 			<button onClick={() => this.submitFile()}>
 			Click to upload file
 			</button>
--- Server/server.js
+++ Server/server.js
@@ -2,11 +2,34 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
+const multer  = require('multer')
+const upload = multer({dest: 'uploads/'})
+
+//Requiring built in node module to read files
+const fs = require('fs')
+
+//Sync and require database
+const db = require('./db_config')
 
 // App level middleware
-app.use(bodyParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(morgan('dev'))
 app.use(express.static('Public'))
+
+// console.log('db', db.models.files)
+
+app.post('/api/uploadFile', upload.single('file'), (req,res) => {
+	console.log('req.file', req.file)
+	fs.readFile(req.file.path, (err, buffer) => {
+		if(err) console.log('err', err)
+    	console.log('buffer', buffer.toString().split('\n'))
+  	})
+	res.end()
+})
+
+
+
 
 const port = process.env.PORT || 3000
 
