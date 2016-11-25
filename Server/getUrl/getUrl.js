5ae69d02a2099cc0f7415dec4378041466f743ea
Warren Spencer <warrenspencer1977@gmail.com>
2016-11-25 10:33:45 -0500

    Upload now works

--- Server/getUrl/getUrl.js
+++ Server/getUrl/getUrl.js
@@ -1,28 +1,59 @@
 /*======================================================================================================
 * 
-*	uploadExpenses.js 	AWS Lambda handler for expense file upload
+*	getUrl.js 	AWS Lambda handler: Accepts nothing, returns an upload Url
 *
 *   21 Nov 2016 Warren Spencer
 *
-*   This Lambda hander performs these functions:
+*	Inputs  	None
+*	Returns 	Stringify'd JSon object {
+*					Url: string
+*					Uuid: string
+*				}
+*	
+*	Revision History
+*	~~~~~~~~~~~~~~~~
 *
-*		Accept an uploaded 
+*	23 Nov 2016  W. Spencer  Created.
 *   
 *======================================================================================================*/
 
-var AWS = require('aws-sdk');
-var UUI = require ("node-uuid");
+const AWS = require("aws-sdk");
+const UUI = require ("node-uuid");
+const URL = require ("url");
+const QRY = require("querystring");
 
 var s3 = new AWS.S3();
 
+// Lambda entry point
 exports.handler = function (event, context, callback) {
 
-	var params = {Bucket: 'wavechallengedata', Key: 'env.js', Expires: 60000};
-	var url = s3.getSignedUrl('putObject', params);
-	//console.log("Put URL is\n" + url);
-	var myObj = {
-		url: url,
-		UUID: UUI.v4()
+	// This UUID will uniquely identify the file within the S3 bucket.  It is
+	// essentially the filename.
+	var filename = UUI.v4();
+
+	// We'll hard code Bucket name and Expires even though they should be pulled form
+	// an environment-specific central config.  This object identifies the S3
+	// bucket (similar to a directory) I've set up to host the uploaded files.
+	var params = {Bucket: 'wavechallengedata', Key: filename, Expires: 600}; // 600 seconds expiry time
+
+	// This url will give the caller a time-limited opportunity to upload a file
+	var s3Url = s3.getSignedUrl('putObject', params);
+
+	// Parse into an object so we can reference different parts of the url easily
+	var urlObj = URL.parse(s3Url);
+
+	// Parse the "query" part of the url into individual object properties
+	var params = QRY.parse (urlObj.search.substr(1)); // First character in this string is always "?"); 
+
+	// Return this object to the caller
+	var clientResponse = {
+		  Url: urlObj.protocol + "//" + urlObj.host + urlObj.pathname // https://wavechallengedata.s3.amazonaws.com/filename
+		, AWSAccessKeyId: params.AWSAccessKeyId // AKIAJAFRE63KT2JMTL6Q
+		, Expires: params.Expires // 1480086036
+		, Signature: params.Signature // AbUCQdJVm0zNZiHlqnjtfaGDBcw=
+		, Uuid: filename // ee226a23-4520-4b35-9d92-176c003bb0cb
 	}
-    callback(null, JSON.stringify (myObj));
+
+	// We're done. Call the Lambda exit  handler - it will stringify clientResponse and return it to the caller
+    callback(null, clientResponse);
 }
