f66f1bd974d82b05fec2c1fc1668db04d19308a5
Alessandro Marin <alessandro.m316@gmail.com>
2016-12-21 18:28:31 -0500

    adds script to generate databse on init if it doesn't exist

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 # -*- encoding: utf-8 -*-
+import os.path
+from app.models import *
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+if not os.path.isfile('./expenses.db'):
+    db.create_all()
