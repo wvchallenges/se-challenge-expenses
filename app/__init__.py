834e8dde465ed861f041cc2e9e6cc807e7e78907
Alessandro Marin <alessandro.m316@gmail.com>
2016-12-21 18:11:58 -0500

    fixes init file

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////expenses.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
