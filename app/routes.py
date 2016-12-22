4127db83a644ef57ac7ecd91e1069b02749965b9
Alessandro Marin <alessandro.m316@gmail.com>
2016-12-21 19:08:56 -0500

    renames controllers to repositories because that's just what they are

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 # -*- encoding: utf-8 -*-
 from flask import render_template
-from repositories.expense import ExpenseRepository
+from app import app
 
 
 @app.route('/')
-def index:
+def index():
     return render_template('index.html')
 
 
