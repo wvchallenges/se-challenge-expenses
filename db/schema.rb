3389a5373d0b1014cc52dce1f43d033b67d8cbfe
Camila <trmcami@gmail.com>
2017-01-10 23:11:04 -0500

    Use database for session storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170108013618) do
+ActiveRecord::Schema.define(version: 20170110051735) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
   end
 
   create_table "employees", force: :cascade do |t|
-    t.string "name"
+    t.string "name",    null: false
     t.string "address"
   end
 
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20170108013618) do
     t.index ["category_id"], name: "index_expenses_on_category_id"
     t.index ["employee_id"], name: "index_expenses_on_employee_id"
     t.index ["tax_id"], name: "index_expenses_on_tax_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "taxes", force: :cascade do |t|
