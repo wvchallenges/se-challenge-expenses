a02d1c8ea2695fdb21adea7d9f7bce2565ffe13d
robert mcmahon <robert mcmahon>
2017-01-13 13:56:32 -0500

    pull right

--- static/app/js/upload.js
+++ static/app/js/upload.js
@@ -23,7 +23,8 @@ function upload_viewModel(upload_view_obj)
         return self.uploaded && self.expenses_per_month().length == 0;
     }, self);
 
-    
+    console.log(enable_upload())
+    console.log(self.uploaded && self.expenses_per_month().length == 0);
     self.file_select = function(vm, evt){
         console.log(vm, evt);
         console.log(evt.target.value);
