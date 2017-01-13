8ed3b97183b725512dcc9942c696d3f14b5e187e
robert mcmahon <robert mcmahon>
2017-01-13 14:58:30 -0500

    syntax

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from .views import Index, ProcessExpenseFile, ExpensePerMonth
+from .views import Index, ProcessExpenseFile
 
 
 urlpatterns = [
     url(r'^$', Index.as_view(), name='index'),
-    url(r'^monthly', ExpensePerMonth.as_view(), name='index'),
     url(r'^process_expense_file', ProcessExpenseFile.as_view(), name='process_expense_file'),
 ]
