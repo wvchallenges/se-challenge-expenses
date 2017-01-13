5531dd874aad6b296f1954277631173de2b2d5ee
robert mcmahon <robert mcmahon>
2017-01-13 15:17:40 -0500

    syntax

--- api/serializers.py
+++ api/serializers.py
@@ -60,10 +60,10 @@ class ExpenseCatagorySerializer(serializers.HyperlinkedModelSerializer):
         fields = ('pk', 'catagory')
 
     def create(self, validated_data):
-        print ("inside the crete ")
-        print(self.__dict__)
-        print(validated_data)
-        return ExpenseCatagory(**validated_data)
+        expense_catagory = ExpenseCatagory.object.filter(expense_catagory=validated_data["catagory"])
+        if len(expense_catagory):
+            return expense_catagory[0]
+        return ExpenseCatagory(expense_catagory=validated_data["catagory"])
 
 
 
