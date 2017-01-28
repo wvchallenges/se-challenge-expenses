e55c59812dc641f66a43d4eef201d5f7ffbaeaac
Geronimo De Abreu <geronimodeabreu@gmail.com>
2017-01-27 19:01:33 -0500

    Making expenses_files#index the application root

--- app/models/expenses_file.rb
+++ app/models/expenses_file.rb
@@ -1,5 +1,5 @@
 class ExpensesFile < ApplicationRecord
-  has_many :expenses
+  has_many :expenses, dependent: :destroy
 
   has_attached_file :file
   validates_attachment :file, presence: true, :content_type => { content_type: 'text/csv' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :expenses_files
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  root "expenses_files#index"
 end
