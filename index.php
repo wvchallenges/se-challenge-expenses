8ce21d9da13016aa8b56334a46892d4a02157cf2
ernan <ernanhughes@gmail.com>
2017-02-06 09:27:45 -0500

    update to working

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<table>
 <?php
-	mysql_connect("localhost", "root", "") or
-    die("Could not connect: " . mysql_error());
-	mysql_select_db("db");
-
-$result = mysql_query("SELECT * FROM EXPENSE");
-
-while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    ?>
-<tr>
-    <td><?=$row['SALE_DATE']; ?></td>
-    <td><?=$row['CATEGORY']; ?></td>
-    <td><?=$row['EMPLOYEE_NAME']; ?></td>
-    <td><?=$row['PRE_TAX_AMOUNT']; ?></td>
-    <td><?=$row['TAX_AMOUNT']; ?></td>
-</tr>    
-<?
-}
-
-mysql_free_result($result);
+    $mysql = new mysqli("localhost", "root");
+    echo "MySQL Server info: ".$mysql->host_info;
 ?>
-</table>
