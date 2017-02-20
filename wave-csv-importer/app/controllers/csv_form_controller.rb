eac10c6372245cb5f20f52b88d3ff279323caf96
drd <treaclywheat@gmail.com>
2017-02-20 13:52:03 -0500

    Added some comments

--- wave-csv-importer/app/controllers/csv_form_controller.rb
+++ wave-csv-importer/app/controllers/csv_form_controller.rb
@@ -8,12 +8,18 @@ class CsvFormController < ApplicationController
   def upload
     parse_csv(params.require(:file))
     render 'csv_form/summary'
+  # rescue all errors and send the user to an error page.
+  # Ideally would handle errors more specifically
+  rescue
+    render 'csv_form/error'
   end
 
   private
 
+  # Given a file, will verify the file is of type csv and attempt to import
+  # its data to the db
   def parse_csv(uploaded_file)
-    fail 'Wrong file type' unless uploaded_file.content_type == 'text/csv'
+    fail unless uploaded_file.content_type == 'text/csv'
 
     csv = CSV.parse(uploaded_file.tempfile, :headers => true)
     csv.each do |row|
@@ -21,23 +27,32 @@ class CsvFormController < ApplicationController
     end
   end
 
+  # Given a row, will attempt to extract the User, Category, and Expense data
+  # from that row. If any creation fails, the import is aborted and the db is
+  # rolled back.
+  # TODO: Currently this creates many db connections, 3 for every row. This
+  # should be extracted out and a bulk_insert of the Users, Categories, and
+  # Expenses should be done instead.
   def process_row(row)
-    # TODO: Wrap in transaction
-    employee = Employee.find_or_create_by!(name: row["employee name"], address: row["employee address"])
-    category = Category.find_or_create_by!(name: row["category"])
-    expense = Expense.create!(
-      # This date parsing is fragile
-      occurance_date: Date.strptime(row["date"],"%m/%d/%Y").to_time,
-      description: row["expense description"],
-      pre_tax_amount: convert_currency_to_number(row["pre-tax amount"]),
-      tax_amount: convert_currency_to_number(row["tax amount"]),
-      tax_type: row["tax name"],
-      employee_id: employee.id,
-      category_id: category.id
-    )
+    # Wrap in transaction to roll back the import if there are data errors
+    ActiveRecord::Base.transaction do
+      employee = Employee.find_or_create_by!(name: row["employee name"], address: row["employee address"])
+      category = Category.find_or_create_by!(name: row["category"])
+      expense = Expense.create!(
+        # This date parsing is fragile, and relies on a set format
+        occurance_date: Date.strptime(row["date"],"%m/%d/%Y").to_time,
+        description: row["expense description"],
+        pre_tax_amount: convert_currency_to_number(row["pre-tax amount"]),
+        tax_amount: convert_currency_to_number(row["tax amount"]),
+        tax_type: row["tax name"],
+        employee_id: employee.id,
+        category_id: category.id
+      )
+    end
   end
 
   # http://stackoverflow.com/questions/24173237/currency-to-number-conversion-rails
+  # Handle special characters that can occur in a stringified currency amount
   def convert_currency_to_number(currency)
     currency.to_s.gsub(/[$,]/,'').to_f
   end
--- wave-csv-importer/app/models/expense.rb
+++ wave-csv-importer/app/models/expense.rb
@@ -1,22 +1,20 @@
 class Expense < ApplicationRecord
   class << self
     def monthly_expenses
+      # Select the date, pre-tax, and tax amount for all Expenses.
       expenses = Expense.all.pluck(:occurance_date, :pre_tax_amount, :tax_amount)
       calculated_monthly_expenses = {}
 
       expenses.each do |expense|
-        month = expense.first.month
-        pre_tax_amount = expense.second
-        tax_amount = expense.third
-
-        subtotal_expense_value = (pre_tax_amount - tax_amount)
-        if calculated_monthly_expenses[month].nil?
-          calculated_monthly_expenses[month] = subtotal_expense_value
+        # expense.second is the pre_tax_amount, expense.third is the tax_amount
+        expense_value = (expense.second - expense.third)
+        time = expense.first.strftime('year: %y month: %m')
+        if calculated_monthly_expenses[time].nil?
+          calculated_monthly_expenses[time] = expense_value
         else
-          calculated_monthly_expenses[month] += subtotal_expense_value
+          calculated_monthly_expenses[time] += expense_value
         end
       end
-
       return calculated_monthly_expenses
     end
   end
