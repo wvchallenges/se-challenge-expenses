b1b9ff51925b1f48b6cace16d987147b0dbfdddc
Zack Mullaly <zsck@riseup.net>
2017-12-15 13:59:39 -0500

    Created a Home component with links to other components that do useful stuff

---
+++ client/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h1>X-Pensive</h1>
+  <p>
+    Take a deep dive into your company's employee expense data.
+  </p>
+  <div>
+    <router-link to="/dashboard">Expenses dashboard</router-link>
+  </div>
+  <div>
+    <router-link to="/upload-report">Upload expense report</router-link>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
