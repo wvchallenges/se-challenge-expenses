f2bdfbab2e274d3b41a6ee5e926503aa83c6c126
Zack Mullaly <zsck@riseup.net>
2017-12-16 18:12:18 -0500

    Progress commit; implemented extract functions to get data out of records

--- server/api/upload_expense_report.go
+++ server/api/upload_expense_report.go
@@ -1,11 +1,30 @@
 package api
 
 import (
-	"encoding/json"
+	"errors"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/zsck/se-challenge-expenses/server/model"
 )
+
+const defaultFileSize int64 = 1024 * 1024 // 1MB
+
+// The name of the key in the request form that should contain the uploaded CSV file.
+const formKey string = "report"
+
+var csvFileHeaders = []string{
+	"date",
+	"category",
+	"employee name",
+	"employee address",
+	"expense description",
+	"pre-tax amount",
+	"tax name",
+	"tax amount",
+}
 
 // ExpenseReportUploader is an HTTP request handler that will:
 //
@@ -16,6 +35,10 @@ import (
 type ExpenseReportUploader struct {
 	employees model.EmployeeLedger
 	expenses  model.ExpenseLedger
+
+	// MaxFileSize is the maximum number of bytes of memory to store uploaded files in.
+	// As per the Golang documentation, files exceeding this size will be partially stored on disk.
+	MaxFileSize int64
 }
 
 // uploadExpenseReportResponse is a simple container for the response produced by the ExpenseReportUploader,
@@ -30,13 +53,93 @@ func NewExpenseReportUploader(employees model.EmployeeLedger, expenses model.Exp
 	return ExpenseReportUploader{
 		employees,
 		expenses,
+		defaultFileSize,
 	}
 }
 
+// ServeHTTP handles file uploads containing CSV files with employee and expense data in them.
+//
+// CSV files are expected to have the following headers:
+// date,category,employee name,employee address,expense description,pre-tax amount,tax name,tax amount
 func (handler ExpenseReportUploader) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "application/json")
-	toClient := json.NewEncoder(res)
-	toClient.Encode(uploadExpenseReportResponse{
-		Error: nil,
-	})
+}
+
+// extractEntities sifts thorugh the plain string records parsed from an uploaded CSV file and writes any employee
+// and expense data it is able to extract from each record through the provided channels.
+func extractEntities(
+	csvRecords [][]string,
+	employees chan<- model.Employee,
+	expenses chan<- model.Expense,
+	errs chan<- error,
+) {
+	for _, record := range csvRecords {
+		employee, err := extractEmployee(record)
+		if err != nil {
+			errs <- err
+		} else {
+			employees <- employee
+		}
+
+		expense, err := extractExpense(record)
+		if err != nil {
+			errs <- err
+		} else {
+			expenses <- expense
+		}
+	}
+	close(employees)
+	close(expenses)
+	close(errs)
+}
+
+func extractEmployee(record []string) (model.Employee, error) {
+	if len(record) < len(csvFileHeaders) {
+		return model.Employee{}, errors.New("not enough columns in record")
+	}
+	employee := model.Employee{
+		ID:      0,
+		Name:    record[2],
+		Address: record[3],
+	}
+	return employee, nil
+}
+
+func extractExpense(record []string) (model.Expense, error) {
+	// Parse the pre- and tax amounts.
+	if len(record) < len(csvFileHeaders) {
+		return model.Expense{}, errors.New("not enough columns in record")
+	}
+	preTaxAmount, parseErr := strconv.ParseFloat(record[5], 64)
+	if parseErr != nil {
+		return model.Expense{}, parseErr
+	}
+	taxAmount, parseErr := strconv.ParseFloat(record[7], 64)
+	if parseErr != nil {
+		return model.Expense{}, parseErr
+	}
+
+	// Parse the date.
+	dateParts := strings.Split(record[0], "/")
+	if len(dateParts) != 3 {
+		return model.Expense{}, errors.New("invalid date format")
+	}
+	month, err1 := strconv.Atoi(dateParts[0])
+	day, err2 := strconv.Atoi(dateParts[1])
+	year, err3 := strconv.Atoi(dateParts[2])
+	if err1 != nil || err2 != nil || err3 != nil {
+		return model.Expense{}, errors.New("invalid date format")
+	}
+
+	utc, _ := time.LoadLocation("UTC")
+	expense := model.Expense{
+		ID:           0,
+		SubmittedBy:  0,
+		Date:         time.Date(year, time.Month(month), day, 0, 0, 0, 0, utc),
+		Category:     record[1],
+		Description:  record[4],
+		PreTaxAmount: preTaxAmount,
+		TaxName:      record[6],
+		TaxAmount:    taxAmount,
+	}
+	return expense, nil
 }
