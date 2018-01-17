236ceae60a6d458b00dab962a5cdaa017e93cce1
John Wood <john.wood@razorfish.com>
2018-01-17 16:25:23 -0500

    Added initial query setup

--- index.js
+++ index.js
@@ -10,29 +10,103 @@ const upload = multer({
   inMemory: true
 }); 
 
-// open database in memory
-var db = new sqlite3.Database('sample.db', (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
-});
-   
-db.run('CREATE TABLE expenses(name text)', [], function(err) {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Table created');
-});
+var thing = function(result) {
+    // open database in memory
+    var db = new sqlite3.Database('expenses.db', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Connected to the SQlite database.');
+    });
 
-// close the database connection
-db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-});
+    var stuff =
+        `CREATE TABLE expenses (
+            expense_id integer PRIMARY KEY,
+            date integer NOT NULL,
+            category text NOT NULL,
+            employee_name text NOT NULL,
+            employee_address text NOT NULL,
+            expense_description text NOT NULL,
+            pre_tax_amount float NOT NULL,
+            tax_name text NOT NULL,
+            tax_amount float NOT NULL
+        )`;
 
+    var other =
+        `INSERT INTO expenses (
+            date,
+            category,
+            employee_name,
+            employee_address,
+            expense_description,
+            pre_tax_amount,
+            tax_name,
+            tax_amount
+        ) VALUES (
+            1,
+            "a",
+            "b",
+            "c",
+            "d",
+            1.0,
+            "e",
+            2.0
+        )`;
+
+    var getIt =
+        `SELECT
+            date,
+            category,
+            employee_name,
+            employee_address,
+            expense_description,
+            pre_tax_amount,
+            tax_name,
+            tax_amount
+        FROM
+            expenses`;
+    
+    /*db.run(stuff, [], function(err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Table created');
+    });*/
+
+    db.serialize(() => {
+        // Queries scheduled here will be serialized.
+        db.run(stuff,[], function(err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log('Table created');
+        })
+        .run(other, [], function(err) {
+            if (err) {
+              return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        })
+        .all(getIt, [], function(err, rows) {
+            if (err) {
+                throw err;
+            }
+            rows.forEach((row) => {
+                console.log(row.tax_amount);
+              });
+        })
+    });
+
+    // close the database connection
+    db.close((err) => {
+        if (err) {
+        return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+}
+thing();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -40,12 +114,32 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', upload.single('upload-file'), (req, res) => {
-    var csvString = req.file.buffer.toString()
-    csv().fromString(csvString, function(err,result){
-        if(err)return res.send("ERR")
+    var csvString = req.file.buffer.toString();
+    var result = [];
+    csv({noheader:false}).fromString(csvString)
+    .on('csv', (row)=> {
+        result.push(row);
+    })
+    .on('done', (err)=> {
+        if (err) return res.send("ERR");
+        thing(result);
         res.send(result);
     }); 
+    /*, function(err,result) {
+        if(err)return res.send("ERR")
+        res.send(result);
+    }); */
 
+/*.on('csv', (row)=> {
+        console.log(row);
+    })
+    .on('done', (err,result)=> {
+        if(err)return res.send("ERR")
+        console.log(result);
+        
+        res.send(result);
+    }); 
+*/
 
 });
 
