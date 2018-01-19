bf6f0728d334250779c9b13a42e83feabce2a8e0
John Wood <john.wood@razorfish.com>
2018-01-19 16:33:41 -0500

    Lots of cleanup. Needs a bit more and some testing.

--- index.js
+++ index.js
@@ -3,155 +3,146 @@ const multer = require('multer');
 const csv=require('csvtojson');
 const sqlite3 = require('sqlite3');
 
-var storage = multer.memoryStorage()
+const storage = multer.memoryStorage()
 const upload = multer({
-  dest: 'uploads/',
-  storage: storage,
   inMemory: true
 }); 
 
-var month = new Array();
-month[0] = "January";
-month[1] = "February";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
-
-var thing = function(result, res) {
-    // open database in memory
-    var db = new sqlite3.Database('expenses.db', (err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Connected to the SQlite database.');
-    });
-
-    var stuff =
-        `CREATE TABLE expenses (
-            expense_id integer PRIMARY KEY,
-            date integer NOT NULL,
-            category text NOT NULL,
-            employee_name text NOT NULL,
-            employee_address text NOT NULL,
-            expense_description text NOT NULL,
-            pre_tax_amount float NOT NULL,
-            tax_name text NOT NULL,
-            tax_amount float NOT NULL
-        )`;
-
-    var other =
-        `INSERT INTO expenses (
-            date,
-            category,
-            employee_name,
-            employee_address,
-            expense_description,
-            pre_tax_amount,
-            tax_name,
-            tax_amount
-        ) VALUES (
-            ?, ?, ?, ?, ?, ?, ?, ?
-        )`;
-
-    var getIt =
-        `SELECT
-            date,
-            pre_tax_amount
-        FROM
-            expenses
-        ORDER BY
-            expense_id DESC
-        LIMIT
-            ${result.length}`;
-
-    // Queries scheduled here will be serialized.
-    db.run(stuff,[], function(err) {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Table created');
-    });
-
-    result.forEach((row) => {
-        db.run(other, row, function(err) {
-            if (err) {
-                return console.log(err.message);
-            }
-            // get the last insert id
-            console.log(`A row has been inserted with rowid ${this.lastID}`);
-        });
-
-    });
-    
-var output = [];
-    db.all(getIt, [], function(err, rows) {
-        if (err) {
-            throw err;
-        }
-        rows.forEach((row) => {
-            row.date = new Date(row.date);
-            row.pre_tax_amount = parseFloat(row.pre_tax_amount.toString().replace(/,/g, ''));
-            output.push(row);
-        });
-        output.sort(function(a, b) {
-            return a.date - b.date;
-        });
-
-        var out = [];
-out.push(output.shift());
-var thing = 0;
-output.forEach(function(row, index) {
-    if (row.date.getMonth() === out[thing].date.getMonth() && row.date.getYear() === out[thing].date.getYear()) {
-        out[thing].pre_tax_amount += row.pre_tax_amount;
-    } else {
-        out[thing].date = month[out[thing].date.getMonth()] + ' ' + out[thing].date.getFullYear();
-        thing++;
-        out.push(row);
-    }
-})
-out[thing].date = month[out[thing].date.getMonth()] + ' ' + out[thing].date.getFullYear();
-
-res.render('output', {data: out});
-        
-    });
-
-
-
-    // close the database connection
-    db.close((err) => {
-        if (err) {
-        return console.error(err.message);
-        }
-        console.log('Close the database connection.');
-    });
-}
+const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const createQuery =
+    `CREATE TABLE expenses (
+        expense_id integer PRIMARY KEY,
+        date integer NOT NULL,
+        category text NOT NULL,
+        employee_name text NOT NULL,
+        employee_address text NOT NULL,
+        expense_description text NOT NULL,
+        pre_tax_amount float NOT NULL,
+        tax_name text NOT NULL,
+        tax_amount float NOT NULL
+    )`;
+const insertQuery =
+    `INSERT INTO expenses (
+        date,
+        category,
+        employee_name,
+        employee_address,
+        expense_description,
+        pre_tax_amount,
+        tax_name,
+        tax_amount
+    ) VALUES (
+        ?, ?, ?, ?, ?, ?, ?, ?
+    )`;
+const selectQuery =
+    `SELECT
+        date,
+        pre_tax_amount
+    FROM
+        expenses
+    ORDER BY
+        expense_id DESC
+    LIMIT
+        `;
 
 const app = express();
 app.engine('pug', require('pug').__express);
-//app.set('views', '/Users/johwood/Sites/se-challenge-expenses/views');
 app.set('view engine', 'pug');
+
+var processData = function(csvData, res) {
+
+    // Connect to or create database
+    var db = new sqlite3.Database('expenses.db', (error) => {
+        if (error) {
+            return console.error(error.message);
+        }
+    });
+
+    // Serialize database actions so they run in order
+    db.serialize(() => {
+        
+        // Create table it if does not exist yet
+        db.run(createQuery, [], (error) => {
+            if (error) {
+                return console.error(error.message);
+            }
+        });
+
+        // Insert the data from the csv file into the database
+        csvData.forEach((row) => {
+            db.run(insertQuery, row, (error) => {
+                if (error) {
+                    return console.log(error.message);
+                }
+            });
+        });
+    
+        // Query the data back from the database, retreiving only the same number of rows as what inserted
+        db.all(selectQuery + csvData.length, [], function(error, dataRows) {
+            if (error) {
+                return console.log(error.message);
+            }
+
+            // Format data into JS Date Objects and Floats
+            let formattedData = [];
+            dataRows.forEach((row) => {
+                row.date = new Date(row.date);
+                row.pre_tax_amount = parseFloat(row.pre_tax_amount.toString().replace(/,/g, ''));
+                formattedData.push(row);
+            });
+            
+            //Sort data
+            formattedData.sort((a, b) => {
+                return a.date - b.date;
+            });
+
+            // Create output array and push in first entry of formatted data
+            let outputData = [];
+            var outputIndex = 0;
+            outputData.push(formattedData.shift());
+            
+            // Iterate through remaining rows:
+            //   - if month is the same then add expense amount into output data
+            //   - if not then create a new entry in output data
+            formattedData.forEach((row) => {
+                if (row.date.getMonth() === outputData[outputIndex].date.getMonth() && row.date.getYear() === outputData[outputIndex].date.getYear()) {
+                    outputData[outputIndex].pre_tax_amount += row.pre_tax_amount;
+                } else {
+                    outputData[outputIndex].date = month[outputData[outputIndex].date.getMonth()] + ' ' + outputData[outputIndex].date.getFullYear();
+                    outputIndex++;
+                    outputData.push(row);
+                }
+            })
+            outputData[outputIndex].date = month[outputData[outputIndex].date.getMonth()] + ' ' + outputData[outputIndex].date.getFullYear();
+
+            // Render output
+            res.render('output', {data: outputData});
+                    
+        });
+    });
+
+    // Close the database connection
+    db.close((error) => {
+        if (error) {
+            return console.error(error.message);
+        }
+    });
+}
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.post('/', upload.single('upload-file'), (req, res) => {
     var csvString = req.file.buffer.toString();
-    var result = [];
+    var csvData = [];
     csv({noheader:false}).fromString(csvString)
     .on('csv', (row)=> {
-        result.push(row);
+        csvData.push(row);
     })
     .on('done', (err)=> {
         if (err) return res.send("ERR");
-        thing(result, res);
-
+        processData(csvData, res);
     }); 
 });
 
