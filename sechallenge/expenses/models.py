774f5a20d5c9dcdaeb9f4c166e2c01a5eb73318b
carolyz <13carol.y.z@gmail.com>
2018-02-26 00:40:29 -0500

    Add basic D3 viz code Add instructions to index Update cols in db to take up less memory

--- sechallenge/expenses/models.py
+++ sechallenge/expenses/models.py
@@ -3,15 +3,12 @@ from django.db import models
 
 class Expenses(models.Model):
     expense_id = models.AutoField(primary_key=True)
+    filename = models.CharField(max_length=50, blank=True)
     date = models.DateField()
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     pretax = models.BigIntegerField()
-    taxname = models.CharField(max_length=100)
+    taxname = models.CharField(max_length=50)
     tax = models.BigIntegerField()
-
-    def __str__(self):
-        return self.name
-
--- sechallenge/expenses/views.py
+++ sechallenge/expenses/views.py
@@ -17,37 +17,42 @@ def index(request):
 def upload_csv(request):
     if request.method == 'POST' and request.FILES['csv_file']:
         file = request.FILES['csv_file']
+        file_name = file.name[:-4]
         if not file.name.endswith('.csv'):
             messages.error(request, 'File is not a CSV')
-        decoded_file = file.read().decode('utf-8').splitlines()
-        reader = csv.DictReader(decoded_file)
-        for row in reader:
-            new_record = Expenses(
-                date=datetime.datetime.strptime(row['date'], '%m/%d/%Y').date(),
-                category=row['category'],
-                name=row['employee name'],
-                address=row['employee address'],
-                description=row['expense description'],
-                pretax=float(row['pre-tax amount'].replace(',', '')),
-                taxname=row['tax name'],
-                tax=float(row['tax amount'].replace(',', '')),
-            )
-            try:
-                new_record.save()
-            except Exception as e:
-                messages.error(request, 'Unable to upload file. ' + repr(e))
+        # check if file already exists in db
+        elif Expenses.objects.filter(filename=file_name).first():
+            messages.error(request, 'File already exists. Try a different one')
+        else:
+            decoded_file = file.read().decode('utf-8').splitlines()
+            reader = csv.DictReader(decoded_file)
+            for row in reader:
+                new_record = Expenses(
+                    filename=file_name,
+                    date=datetime.datetime.strptime(row['date'], '%m/%d/%Y').date(),
+                    category=row['category'],
+                    name=row['employee name'],
+                    address=row['employee address'],
+                    description=row['expense description'],
+                    pretax=float(row['pre-tax amount'].replace(',', '')),
+                    taxname=row['tax name'],
+                    tax=float(row['tax amount'].replace(',', '')),
+                )
+                try:
+                    new_record.save()
+                except Exception as e:
+                    messages.error(request, 'Unable to upload file. ' + repr(e))
     # after data uploaded, calculate monthly expenses
-    return view_monthly_expenses(request)
+    return view_monthly_expenses(request, file_name)
 
 
 # fetches stored model & display
-def view_monthly_expenses(request):
+def view_monthly_expenses(request, file_name):
+    # try to get all the expenses with a particular file name, agg by month
     try:
-        expenses_list = Expenses.objects.annotate(month=TruncMonth('date')).values('month').annotate(s=Sum('pretax'))
+        expenses_list = Expenses.objects.filter(filename=file_name).annotate(month=TruncMonth('date')).values('month').annotate(s=Sum('pretax'))
     except Expenses.DoesNotExist:
         raise Http404("Expense does not exist")
-    # if data successfully uploaded, calculate expenses by month
-
     context = {'expenses_list': expenses_list}
     return render(request, 'expenses/upload_csv.html', context)
 
